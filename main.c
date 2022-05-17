49d5621db55a571abafe0b3f3387d928935ffec0
T0KICH0 <nickhailut@gmail.com>
2022-05-17 10:33:53 -0700

    edit main.c more

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 *
 * Return: 0 after printing the function
 */
-int main(void){
+int main(void)
+{
 char Gpa = 'A';
 char Name[] = "John";
 printf("hello %s\n", Name);
 printf("your Gpa is %c\n", Gpa);
-return 0;
+return (0);
 }
 
