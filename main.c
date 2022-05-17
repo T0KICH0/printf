d57109a3719c6a9c73b220d48d4c511ebd1f3915
T0KICH0 <nickhailut@gmail.com>
2022-05-17 10:25:08 -0700

    edited main.c

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main - entry point
- *
- * Return: 0 after printing the function
- */
+* main - entry point
+*
+* Return: 0 after printing the function
+*/
 int main(void){
-	printf("hello");
-	return 0;
+char Gpa = 'A';
+char Name[] = "John";
+printf("hello %s your grade is %c", Name, Gpa);
+return 0;
 }
+
