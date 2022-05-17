dad8e7fa26ebaea65a1df2f6a5a7e7b524b687fa
T0KICH0 <nickhailut@gmail.com>
2022-05-18 00:47:54 +0300

    added

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 after printing the function
+*/
+int main(void)
+{
+char Gpa = 'A';
+char Name[] = "John";
+printf("hello %s\n", Name);
+printf("your Gpa is %c\n", Gpa);
+return (0);
+}
+
